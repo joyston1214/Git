dc6e8d5b72e7ddb5017a3b2cdbbc490f37a40964
joyston1214 <fjoyston@gmail.com>
2023-11-30 09:24:35 +0530

    commit

--- src/test/java/Git/Git_Bash.java
+++
@@ -1,8 +0,0 @@
-package Git;
-
-public class Git_Bash {
-	public static void main(String[] args) {
-		System.out.println("git");
-	}
-
-}
---
+++ src/test/java/Git/Git_Bash_Test.java
@@ -0,0 +1,12 @@
+package Git;
+
+import org.testng.annotations.Test;
+
+public class Git_Bash_Test {
+	
+	@Test
+	public  void jenkins_Test() {
+		System.out.println("jenkins");
+	}
+
+}
